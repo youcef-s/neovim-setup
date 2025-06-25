bbd86a97eae69bdaa25a67ebec05b3be4067aced
Youssef Labtaimi <oyoucef099@gmail.com>
2025-06-25 13:42:16 +0100

    added nvim-surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -136,4 +136,12 @@ return {
       })
     end,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end
+  }
 }
